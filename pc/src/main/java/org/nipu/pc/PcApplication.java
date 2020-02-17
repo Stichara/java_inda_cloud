fa10280393d991fb97f1c538c38fb1baba628cbe
Aleh Halaburda <ahalaburda@mobileiron.com>
2020-02-17 23:58:56 +0300

    resolve practical task for microservice course

--- pc/src/main/java/org/nipu/pc/PcApplication.java
+++ pc/src/main/java/org/nipu/pc/PcApplication.java
@@ -2,11 +2,23 @@ package org.nipu.pc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RequestMapping("/pc")
 public class PcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PcApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello";
     }
 }
